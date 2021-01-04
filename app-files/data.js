66243decc2a7bf66a7240c5f3fbd2819b2e60626
Francisco Laguna de la Vera <francisco.laguna@open-xchange.com>
2021-01-04 18:49:04 +0100

    fix ausstattung

--- app-files/data.js
+++ app-files/data.js
@@ -336,7 +336,8 @@ var APP_DATA = {
           "pitch": 0,
           "title": "Computerraum",
           "text": "Digitale Ausstattung",
-          "video": "ausstattung"
+          "video": "ausstattung",
+          "screen": "schwebend"
         },
         {
           "yaw": -2.95,
